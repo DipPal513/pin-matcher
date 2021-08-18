48b25b545db63be0a7b40858394d36a0f5d8070a
DipPal513 <65772938+DipPal513@users.noreply.github.com>
2021-08-18 22:06:27 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,44 +1,49 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
+    // for getting the length of pin
     const pinString = pin + '';
+    //condition
     if (pinString.length == 4) {
         return pin;
     }
     else {
-        // console.log('got 3 digit and calling again', pin);
+        // if it's doesn't find 4 digit then again call 
         return getPin();
     }
 }
-function generatePin() {
-    const pin = getPin();
-    document.getElementById('display-pin').value = pin;
+
+function generatePin(){
+    let pin = getPin()
+    const displayPin = document.getElementById('display-pin');
+    displayPin.value = pin;
 }
 
-document.getElementById('key-pad').addEventListener('click', function (event) {
-    const number = event.target.innerText;
-    const calcInput = document.getElementById('typed-numbers');
-    if (isNaN(number)) {
-        if (number == 'C') {
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+   const selectedNumber = event.target.innerText;
+   const calcInput = document.getElementById('typed-numbers');
+
+    if(isNaN(selectedNumber)){
+        if(selectedNumber == 'C'){
             calcInput.value = '';
         }
     }
-    else {
-        const previousNumber = calcInput.value;
-        const newNumber = previousNumber + number;
-        calcInput.value = newNumber;
+    else{
+        const previousNumber  = calcInput.value;
+        calcInput.value = previousNumber + selectedNumber;
     }
-});
-
-function verifyPin() {
+})
+function verifyPin(){
     const pin = document.getElementById('display-pin').value;
     const typedNumbers = document.getElementById('typed-numbers').value;
     const successMessage = document.getElementById('notify-success');
     const failError = document.getElementById('notify-fail');
-    if (pin == typedNumbers) {
+
+    if(pin == typedNumbers){
         successMessage.style.display = 'block';
         failError.style.display = 'none';
     }
-    else {
+    else{
         successMessage.style.display = 'none';
         failError.style.display = 'block';
     }
